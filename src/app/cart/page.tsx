1208e66d521815c615ca93b1ab0186bb80266d1b
Sadiq Layicov <sadiqlayicov@gmail.com>
2025-08-07 11:01:00 +0300

    Fix clearCart import error in cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -35,6 +35,7 @@ export default function CartPage() {
     refreshCart,
     updateQuantity,
     removeFromCart,
+    clearCart,
     isLoading: cartLoading
   } = useCart();
   const router = useRouter();
